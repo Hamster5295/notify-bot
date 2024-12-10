6b9adf4d3fa6dbfdd3de791ba0dcd4dd38e1c877
Hamster5295 <hamster5295@163.com>
2024-12-10 16:05:02 +0800

    feat: starting title & bugfix

--- src/service.rs
+++ src/service.rs
@@ -58,7 +58,7 @@ async fn handle_notify_request(
     runtime_cfg: &RuntimeConfig,
     client: &Client,
 ) {
-    let mut msg = notify_cfg.message.clone();
+    let mut msg: String = notify_cfg.message.clone();
 
     if notify_cfg.extra.unwrap_or(false) {
         if notify_cfg.extractors.is_some() && notify_cfg.extractors.as_ref().unwrap().len() > 0 {
@@ -85,7 +85,7 @@ async fn handle_notify_request(
                 warn!("Failed to format message with extracted contents.")
             }
         } else {
-            warn!("Extra is set to true, but no extractors are defined.")
+            warn!("EXTRA is set to true, but no extractors are defined.")
         }
     }
     info!(format!("Sending Message: \n\n{}\n", msg));
@@ -202,7 +202,10 @@ fn extract_arg_impl(val: Value, paths: &Vec<&str>, sep: &str, idx: usize) -> Opt
             if let Some(res) = res {
                 results.push(res);
             } else {
-                warn!(format!("The error occurred at index \x1b[1m{}\x1b[0m of the array", i));
+                warn!(format!(
+                    "The error occurred at index \x1b[1m{}\x1b[0m of the array",
+                    i
+                ));
             }
         }
 
@@ -288,6 +291,218 @@ mod tests {
         assert_eq!(
             extract_arg(&val, &"nesting-list.[].special".to_string(), sep).unwrap(),
             "8"
+        );
+    }
+
+    #[test]
+    fn test_github_webhook() {
+        let val = json!({
+          "ref": "refs/heads/main",
+          "before": "1e84324b2c4788b684e53598161ffdcc17d4146d",
+          "after": "671d0e94215730b2d294e280ce2f03996b213323",
+          "repository": {
+            "id": 897667274,
+            "node_id": "R_kgDONYFQyg",
+            "name": "digital-ic-proj",
+            "full_name": "Hamster5295/digital-ic-proj",
+            "private": true,
+            "owner": {
+              "name": "Hamster5295",
+              "email": "37259613+Hamster5295@users.noreply.github.com",
+              "login": "Hamster5295",
+              "id": 37259613,
+              "node_id": "MDQ6VXNlcjM3MjU5NjEz",
+              "avatar_url": "https://avatars.githubusercontent.com/u/37259613?v=4",
+              "gravatar_id": "",
+              "url": "https://api.github.com/users/Hamster5295",
+              "html_url": "https://github.com/Hamster5295",
+              "followers_url": "https://api.github.com/users/Hamster5295/followers",
+              "following_url": "https://api.github.com/users/Hamster5295/following{/other_user}",
+              "gists_url": "https://api.github.com/users/Hamster5295/gists{/gist_id}",
+              "starred_url": "https://api.github.com/users/Hamster5295/starred{/owner}{/repo}",
+              "subscriptions_url": "https://api.github.com/users/Hamster5295/subscriptions",
+              "organizations_url": "https://api.github.com/users/Hamster5295/orgs",
+              "repos_url": "https://api.github.com/users/Hamster5295/repos",
+              "events_url": "https://api.github.com/users/Hamster5295/events{/privacy}",
+              "received_events_url": "https://api.github.com/users/Hamster5295/received_events",
+              "type": "User",
+              "user_view_type": "public",
+              "site_admin": false
+            },
+            "html_url": "https://github.com/Hamster5295/digital-ic-proj",
+            "description": "数集课设",
+            "fork": false,
+            "url": "https://github.com/Hamster5295/digital-ic-proj",
+            "forks_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/forks",
+            "keys_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/keys{/key_id}",
+            "collaborators_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/collaborators{/collaborator}",
+            "teams_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/teams",
+            "hooks_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/hooks",
+            "issue_events_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/issues/events{/number}",
+            "events_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/events",
+            "assignees_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/assignees{/user}",
+            "branches_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/branches{/branch}",
+            "tags_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/tags",
+            "blobs_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/git/blobs{/sha}",
+            "git_tags_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/git/tags{/sha}",
+            "git_refs_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/git/refs{/sha}",
+            "trees_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/git/trees{/sha}",
+            "statuses_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/statuses/{sha}",
+            "languages_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/languages",
+            "stargazers_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/stargazers",
+            "contributors_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/contributors",
+            "subscribers_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/subscribers",
+            "subscription_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/subscription",
+            "commits_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/commits{/sha}",
+            "git_commits_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/git/commits{/sha}",
+            "comments_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/comments{/number}",
+            "issue_comment_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/issues/comments{/number}",
+            "contents_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/contents/{+path}",
+            "compare_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/compare/{base}...{head}",
+            "merges_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/merges",
+            "archive_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/{archive_format}{/ref}",
+            "downloads_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/downloads",
+            "issues_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/issues{/number}",
+            "pulls_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/pulls{/number}",
+            "milestones_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/milestones{/number}",
+            "notifications_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/notifications{?since,all,participating}",
+            "labels_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/labels{/name}",
+            "releases_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/releases{/id}",
+            "deployments_url": "https://api.github.com/repos/Hamster5295/digital-ic-proj/deployments",
+            "created_at": 1733194602,
+            "updated_at": "2024-12-10T06:04:19Z",
+            "pushed_at": 1733811315,
+            "git_url": "git://github.com/Hamster5295/digital-ic-proj.git",
+            "ssh_url": "git@github.com:Hamster5295/digital-ic-proj.git",
+            "clone_url": "https://github.com/Hamster5295/digital-ic-proj.git",
+            "svn_url": "https://github.com/Hamster5295/digital-ic-proj",
+            "homepage": null,
+            "size": 7,
+            "stargazers_count": 0,
+            "watchers_count": 0,
+            "language": "Verilog",
+            "has_issues": true,
+            "has_projects": true,
+            "has_downloads": true,
+            "has_wiki": false,
+            "has_pages": false,
+            "has_discussions": false,
+            "forks_count": 0,
+            "mirror_url": null,
+            "archived": false,
+            "disabled": false,
+            "open_issues_count": 0,
+            "license": null,
+            "allow_forking": true,
+            "is_template": false,
+            "web_commit_signoff_required": false,
+            "topics": [
+
+            ],
+            "visibility": "private",
+            "forks": 0,
+            "open_issues": 0,
+            "watchers": 0,
+            "default_branch": "main",
+            "stargazers": 0,
+            "master_branch": "main"
+          },
+          "pusher": {
+            "name": "Hamster5295",
+            "email": "37259613+Hamster5295@users.noreply.github.com"
+          },
+          "sender": {
+            "login": "Hamster5295",
+            "id": 37259613,
+            "node_id": "MDQ6VXNlcjM3MjU5NjEz",
+            "avatar_url": "https://avatars.githubusercontent.com/u/37259613?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/Hamster5295",
+            "html_url": "https://github.com/Hamster5295",
+            "followers_url": "https://api.github.com/users/Hamster5295/followers",
+            "following_url": "https://api.github.com/users/Hamster5295/following{/other_user}",
+            "gists_url": "https://api.github.com/users/Hamster5295/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/Hamster5295/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/Hamster5295/subscriptions",
+            "organizations_url": "https://api.github.com/users/Hamster5295/orgs",
+            "repos_url": "https://api.github.com/users/Hamster5295/repos",
+            "events_url": "https://api.github.com/users/Hamster5295/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/Hamster5295/received_events",
+            "type": "User",
+            "user_view_type": "public",
+            "site_admin": false
+          },
+          "created": false,
+          "deleted": false,
+          "forced": false,
+          "base_ref": null,
+          "compare": "https://github.com/Hamster5295/digital-ic-proj/compare/1e84324b2c47...671d0e942157",
+          "commits": [
+            {
+              "id": "671d0e94215730b2d294e280ce2f03996b213323",
+              "tree_id": "2f14d338b38da4b4169810fa0a0c455ad35b9a11",
+              "distinct": true,
+              "message": "chore: 测试 bot",
+              "timestamp": "2024-12-10T14:15:11+08:00",
+              "url": "https://github.com/Hamster5295/digital-ic-proj/commit/671d0e94215730b2d294e280ce2f03996b213323",
+              "author": {
+                "name": "Hamster5295",
+                "email": "hamster5295@163.com",
+                "username": "Hamster5295"
+              },
+              "committer": {
+                "name": "Hamster5295",
+                "email": "hamster5295@163.com",
+                "username": "Hamster5295"
+              },
+              "added": [
+
+              ],
+              "removed": [
+
+              ],
+              "modified": [
+                "FSM.v"
+              ]
+            }
+          ],
+          "head_commit": {
+            "id": "671d0e94215730b2d294e280ce2f03996b213323",
+            "tree_id": "2f14d338b38da4b4169810fa0a0c455ad35b9a11",
+            "distinct": true,
+            "message": "chore: 测试 bot",
+            "timestamp": "2024-12-10T14:15:11+08:00",
+            "url": "https://github.com/Hamster5295/digital-ic-proj/commit/671d0e94215730b2d294e280ce2f03996b213323",
+            "author": {
+              "name": "Hamster5295",
+              "email": "hamster5295@163.com",
+              "username": "Hamster5295"
+            },
+            "committer": {
+              "name": "Hamster5295",
+              "email": "hamster5295@163.com",
+              "username": "Hamster5295"
+            },
+            "added": [
+
+            ],
+            "removed": [
+
+            ],
+            "modified": [
+              "FSM.v"
+            ]
+          }
+        });
+
+        assert_eq!(
+            extract_arg(&val, &"head_commit.message".to_string(), ",").unwrap(),
+            "chore: 测试 bot"
+        );
+
+        assert_eq!(
+            extract_arg(&val, &"head_commit.committer.name".to_string(), ",").unwrap(),
+            "Hamster5295"
         );
     }
 }
